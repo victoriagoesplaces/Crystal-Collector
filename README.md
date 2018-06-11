5703913c1653bb3cc498b3fd94040fcfa0ff6f7a
Victoria Gonzalez <vmjgr@Victorias-MBP.attlocal.net>
2018-06-10 22:09:38 -0400

    javascript updates

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # unit-4-game
+
+*_Crystal Collector Game_* !
+
+#### How to play?
+
+*  Use the following link to get started [Crystal Collector Game](https://victoriagoesplaces.github.io/unit-4-game/)
+
+You will be given a random target number at the beginnig of the game.
+
+There will be four crystals with random numbers assigned to them.
+
+The value of your current number will be the addition of each clicked crystal.
+
+### How do I win?
+
+If you are able match the target number, an alert will display "You win!" 
+
+on the other hand, if the current number goes over the target number, you will recieve an alert display "Oh Oh, try again!"
+
+You will be able to keep track of the number of games you have lost or won on the. 
+
+#### Remember-
+
+The values of each crystal and target number will change every time the game restarts. 
+
+### Screenshot
+
+![screenshot](assets/images/screenshot.jpg "Screenshot of game")
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -39,19 +39,17 @@ $(".crystal4").on("click", function() {
 
 function reset() {
 
-    var current = 0
+   current = 0
     $("#current").text(current);
 
-    var targetNumber =  Math.floor(Math.random() * (120 - 19)) + 19;
+    targetNumber =  Math.floor(Math.random() * (120 - 19)) + 19;
     $("#target").text(targetNumber);
 
-    var crystal =  Math.floor(Math.random() * 12) + 1;
-    var crystal2 =  Math.floor(Math.random() * 12) + 1;
-    var crystal3 =  Math.floor(Math.random() * 12) + 1;
-    var crystal4 =  Math.floor(Math.random() * 12) + 1;
-    
-    $("#lossCounter").text(lossCounter);
-    $("#winCounter").text(winCounter);}
+    crystal =  Math.floor(Math.random() * 12) + 1;
+    crystal2 =  Math.floor(Math.random() * 12) + 1;
+    crystal3 =  Math.floor(Math.random() * 12) + 1;
+    crystal4 =  Math.floor(Math.random() * 12) + 1;   
+}
 
 $("#resetButton").on("click", function() {
     reset();
@@ -61,15 +59,14 @@ function score() {
 if (current === targetNumber) {
     winCounter++;
     alert("You won!")
-    $("#winCounter").text(winCounter);
-    reset()
+    $("#winCounter").text(winCounter); 
+    
 }
 
 else if (current > targetNumber){
     lossCounter++
     alert("Oh Oh, try again!")
     $("#lossCounter").text(lossCounter);
-    reset()
 }
 }
 
